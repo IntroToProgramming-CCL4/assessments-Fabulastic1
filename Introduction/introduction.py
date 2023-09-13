84d6ff8b0f8141ccc973a3d54b7d95be86fc4d4b
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-09-13 19:24:29 +0400

    Add files via upload

---
+++ Introduction/introduction.py
@@ -0,0 +1,3 @@
+print("Anas imtiaz\n"
+      "im 18 years old and i have a keen interest in programming with the end goal\n"
+      "of making a game someday besides that my hobbies are skateboarding, gaming and photography")
