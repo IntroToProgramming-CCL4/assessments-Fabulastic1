8b5cf9855cd7616ee26770d0116c2a1b02e8626a
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 00:50:41 +0400

    Update Exercise1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -1,1 +1,6 @@
-print('Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are')
+print("""Twinkle, twinkle, little star,          
+              How I wonder what you are!          #at first it gave me errors because of all the sym i simply just had to put
+                      Up above the world so high,  #three commas instead of one to solve the problem
+                      Like a diamond in the sky.
+      Twinkle, twinkle, little star,
+              How I wonder what you are""")
