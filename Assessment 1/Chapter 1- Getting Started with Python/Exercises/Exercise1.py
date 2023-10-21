786d2bc1064ebea05fc34d34e8c58509b5f8a81f
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 00:51:00 +0400

    Update Exercise1.py

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -1,5 +1,5 @@
 print("""Twinkle, twinkle, little star,          
-              How I wonder what you are!          #at first it gave me errors because of all the sym i simply just had to put
+              How I wonder what you are!          #at first it gave me errors because of all the symbols i simply just had to put
                       Up above the world so high,  #three commas instead of one to solve the problem
                       Like a diamond in the sky.
       Twinkle, twinkle, little star,
