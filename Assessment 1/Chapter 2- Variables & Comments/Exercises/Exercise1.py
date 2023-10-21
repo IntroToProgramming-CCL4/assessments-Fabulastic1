7390d21d272b9dbacbc640051bf115ce81530c18
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 01:09:41 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+# assigning a message to a variable
+message = "hello sir, i'm from microsoft your computer has virus send me your bank details"
+# printing the assigned message
+print(message)
+# changing the value of the message
+message = "Hello sir, you have been pranked i just stole all your money lol"
+# printing message again
+print(message)
