efd06e2f2af391f4ef8ef94819736b752ea6f19e
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 01:59:45 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise4.py
@@ -0,0 +1,5 @@
+# storing my favorite number in a variable
+favorite_number = 6
+# creatingg a message to show my favorite number
+message = f"my favorite number is {favorite_number}"
+print(message)
