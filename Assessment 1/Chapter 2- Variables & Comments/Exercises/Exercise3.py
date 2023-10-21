f7f6613a3b5f64529ff1e90a813f2c4b8f736634
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 01:52:29 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,12 @@
+# Storing the name in a variable with whitespaces
+name = '\t\n   tyler Durden   \t\n'
+
+# printing the name with the whitespace
+print(name)
+
+# Printing the name after using all the strip functions
+print(name.lstrip())    # stripping left whitespace
+
+print(name.rstrip())    # stripping right whitespace
+
+print(name.strip())     # stripping whitespaces from both sides
