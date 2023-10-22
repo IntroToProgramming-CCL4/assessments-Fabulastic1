c0b62d9bfc862f13c76b302303924d0ef0c7cfb7
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 18:57:12 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -0,0 +1,5 @@
+# storing the names in a list
+names = ['abraham', 'napoleon', 'stalin', 'philip', 'adolph']
+# using a for loop to print each name separately in a line
+for name in names:
+    print(name)
