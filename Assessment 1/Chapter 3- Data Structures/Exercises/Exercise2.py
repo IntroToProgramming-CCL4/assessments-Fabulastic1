d889dcd893b7fc29912627006ac328152b918513
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-22 19:01:51 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+# storing the names in a list
+names = ['Abraham', 'Napoleon', 'Stalin', 'Philip', 'Adolph']
+# using a for loop to print each name separately in a line
+for name in names:           # writing a personalized message for each of the names
+    message = f'Hello, {name}! have a great lunch'
+    print(message)
