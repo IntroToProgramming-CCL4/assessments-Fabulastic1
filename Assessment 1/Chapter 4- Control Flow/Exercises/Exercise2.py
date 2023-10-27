f5737ff20c34885c6d30a2df4b831ef5f1a61052
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-27 23:08:21 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,11 @@
+# Creating the variable alien color to store the correct color in
+alien_color = "green"  # This is the program that runs the if block
+if alien_color == "green":  # Writing an if statement to make conditions for the statement to be true
+    print("You just earned 5 points for shooting the alien")  # printing the result
+
+# Creating the version of the program that runs the else block
+alien_color = "red"
+if alien_color == "green":
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points")
