596f090d02efc1c6f825f21088bc9ab8cf1d29ab
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-28 01:22:56 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,17 @@
+# Creating a list containing three of my favorite fruits
+favorite_fruits = ['avocados', 'bananas', 'mangoes']
+
+if 'avocados' in favorite_fruits:      # Using if and in statements to check for certain fruits in the list
+    print("You really like Avocados!")  # printing a statement if the fruit is found in the list
+
+if 'bananas' in favorite_fruits:       # Making separate if statements for each of the fruits
+    print("You really like bananas!")
+
+if 'mangoes' in favorite_fruits:
+    print("You really like mangoes!")
+
+if 'kiwi' in favorite_fruits:
+    print("You really like kiwis!")
+
+if 'dragonfruit' in favorite_fruits:
+    print("You really like dragon fruits!")
