75f59d7e3fbc5351423d4d4e5adf1952fa9f944e
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-27 22:02:30 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+# Creating the variable alien color to store the correct color in
+alien_color = "green"
+if alien_color == "green":  # Writing an if statement to make conditions for the statement to be true
+    print("You just earned 5 points")  # printing the result
+
+# Creating the version of the program that fails
+alien_color = "red"
+if alien_color == "green":
+    print("You just earned 5 points")
+else:
+    print()
+  # No output in this version
