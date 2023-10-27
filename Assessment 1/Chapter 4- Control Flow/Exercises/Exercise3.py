414b8d5c40e6c14f0f7b3bed52e710a1442818e5
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-27 23:19:05 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,27 @@
+# Creating the variable alien color to store the correct color in
+alien_color = "green"  # This is the program for the green alien
+if alien_color == "green":  # Writing an if statement to make conditions for the statement to be true
+    print("You just earned 5 points for shooting the alien")  # printing the result
+elif alien_color == "yellow":
+    print("You just earned 10 points for shooting the alien")
+else:
+    print("You just earned 15 points for shooting the alien")
+
+# Creating a version of the program for the yellow alien
+alien_color = "yellow"
+if alien_color == "green":
+    print("You just earned 5 points for shooting the alien")
+elif alien_color == "yellow":
+    print("You just earned 10 points for shooting the alien")
+else:
+    print("You just earned 15 points for shooting the alien")
+
+
+# Creating a version of the program for the red alien
+alien_color = "red"
+if alien_color == "green":
+    print("You just earned 5 points for shooting the alien")
+elif alien_color == "yellow":
+    print("You just earned 10 points for shooting the alien")
+else:
+    print("You just earned 15 points for shooting the alien")
