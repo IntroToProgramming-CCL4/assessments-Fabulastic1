84626b3d341bcc71b3dacdbded87bb73120d427d
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-10-29 18:23:41 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,14 @@
+# Creating a dictionary named person_info to store all their information in
+person_info = {
+    "first_name": "Sponggebob",
+    "last_name": "Squarepants",
+    "age": "Wayy too old",
+    "city": "Bikini bottom"
+}
+
+
+# Printing all the info stored in the dictionary
+print("First Name: " + person_info["first_name"])
+print("Last NameL: " + person_info["last_name"])
+print("Age: " + person_info["age"])
+print("City: ", person_info["city"])
