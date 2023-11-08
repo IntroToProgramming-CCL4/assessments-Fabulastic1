9a4fc395d7ab1ae8a82326746008379555097200
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-11-08 19:13:03 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+# Writing a function called display_message using the def statement
+def display_message():
+    # Printing a statement telling everyone about what i'm learning in this chapter
+    print("In this chapter i'm learning about functions and the def statement")
+# calling upon the function to print the statement
+display_message()
