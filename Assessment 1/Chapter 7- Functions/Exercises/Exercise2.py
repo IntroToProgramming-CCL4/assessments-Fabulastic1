15488e57835666e87ed453427184483ce41a6594
Fabulastic1 <124917123+Fabulastic1@users.noreply.github.com>
2023-11-08 19:33:49 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,9 @@
+# Writing a function called favourite book using the def statement
+def favourite_book(title):
+    # Print a message for the given parameter
+    print("One of my favourite books are", title)
+
+
+# calling the function and specifying the titles of the books
+favourite_book('Charlie and the chocolate factory')
+favourite_book('Diary of a wimpy kid')
